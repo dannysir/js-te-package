9c776706a22efb40272955ba9ab71feacc86d7fe
dannysir <dannysir@naver.com>
2025-11-26 15:11:31 +0900

    refactor : cli.js 파일 내 로직 분리
    파일을 찾는 findFiles 로직과 babel을 통해 변역을 진행하는 transformFiles 파일로 분리
    cli로직을 분리하여 cli 내에서는 전체 흐름만 표현하도록 수정

--- bin/cli.js
+++ bin/cli.js
@@ -1,108 +1,25 @@
 #!/usr/bin/env node
 
-import fs from 'fs';
 import path from 'path';
-import {transformSync} from '@babel/core';
 import * as jsTe from '../index.js';
+import {restoreFiles, transformFiles} from "./utils/transformFiles.js";
+import {findAllSourceFiles, findTestFiles} from "./utils/findFiles.js";
 import {green, red, yellow} from "../utils/consoleColor.js";
 import {getTestResultMsg} from "../utils/makeMessage.js";
-import {BABEL, PATH, RESULT_TITLE} from "../constants.js";
-import { babelTransformImport } from '../babelTransformImport.js';
+import {RESULT_TITLE} from "../constants.js";
 
 let totalPassed = 0;
 let totalFailed = 0;
-const originalFiles = new Map();
 
 Object.keys(jsTe).forEach(key => {
   global[key] = jsTe[key];
 });
 
-const transformFile = (filePath) => {
-  const originalCode = fs.readFileSync(filePath, 'utf-8');
-  originalFiles.set(filePath, originalCode);
-
-  const transformed = transformSync(originalCode, {
-    filename: filePath,
-    plugins: [babelTransformImport],
-    parserOpts: {
-      sourceType: BABEL.MODULE,
-      plugins: ['dynamicImport']
-    }
-  });
-
-  fs.writeFileSync(filePath, transformed.code);
-};
-
-const restoreFiles = () => {
-  for (const [filePath, originalCode] of originalFiles.entries()) {
-    try {
-      fs.writeFileSync(filePath, originalCode);
-    } catch (error) {
-      console.error(red(`Failed to restore ${filePath}: ${error.message}`));
-    }
-  }
-  originalFiles.clear();
-};
-
-const findTestFiles = (dir) => {
-  const files = [];
-
-  const walk = (directory, inTestDir = false) => {
-    const items = fs.readdirSync(directory);
-    const dirName = path.basename(directory);
-
-    const isTestDir = dirName === PATH.TEST_DIRECTORY || inTestDir;
-
-    for (const item of items) {
-      if (item === PATH.NODE_MODULES) continue;
-
-      const fullPath = path.join(directory, item);
-      const stat = fs.statSync(fullPath);
-
-      if (stat.isDirectory()) {
-        walk(fullPath, isTestDir);
-      } else if (item.endsWith(PATH.TEST_FILE) || isTestDir) {
-        if (item.endsWith(PATH.JAVASCRIPT_FILE)) {
-          files.push(fullPath);
-        }
-      }
-    }
-  }
-
-  walk(dir);
-  return files;
-}
-
-const findAllSourceFiles = (dir) => {
-  const files = [];
-
-  const walk = (directory) => {
-    const items = fs.readdirSync(directory);
-
-    for (const item of items) {
-      if (item === PATH.NODE_MODULES || item === PATH.BIN || item === PATH.TEST_DIRECTORY) continue;
-
-      const fullPath = path.join(directory, item);
-      const stat = fs.statSync(fullPath);
-
-      if (stat.isDirectory()) {
-        walk(fullPath);
-      } else if (item.endsWith(PATH.JAVASCRIPT_FILE) && !item.endsWith(PATH.TEST_FILE)) {
-        files.push(fullPath);
-      }
-    }
-  }
-
-  walk(dir);
-  return files;
-}
-
-
 const main = async () => {
   try {
     const sourceFiles = findAllSourceFiles(process.cwd());
     for (const file of sourceFiles) {
-      transformFile(file);
+      transformFiles(file);
     }
 
     const testFiles = findTestFiles(process.cwd());
@@ -112,7 +29,7 @@ const main = async () => {
     for (const file of testFiles) {
       console.log(`\n${yellow(file)}\n`);
 
-      transformFile(file);
+      transformFiles(file);
       await import(path.resolve(file));
 
       const {passed, failed} = await jsTe.run();
